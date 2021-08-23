ae1fefa527e60b0944ec09c98cffa83e24090ec7
zet694 <vitaliy@particle.one>
2021-08-23 22:04:01 +0300

    Add hostname in respornse

--- simple-web-app/main.go
+++ simple-web-app/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, 世界")
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintln(w, "Hostname: ", hostname)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8888", nil))
+	fmt.Println(os.Hostname())
 }
