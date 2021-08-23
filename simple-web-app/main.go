d145c4b3d23be678aaac35f4622a5849cd0ab5ab
zet694 <vitaliy@particle.one>
2021-08-23 22:17:58 +0300

    Update main image for building docker image

--- simple-web-app/main.go
+++ simple-web-app/main.go
@@ -12,7 +12,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintln(w, "Hostname: ", hostname)
+	fmt.Fprintln(w ,"Hostname: ", hostname)
 }
 
 func main() {
