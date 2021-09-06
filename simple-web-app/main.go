941e3873a4df4adb39a7009df45d28557b0f5020
Vitaliy Vrublevskiy <vitaliy@particle.one>
2021-09-06 15:59:13 +0300

    Add port as env variable

--- simple-web-app/main.go
+++ simple-web-app/main.go
@@ -7,16 +7,25 @@ import (
 	"os"
 )
 
+func getenv(key, default_key string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return default_key
+	}
+	return value
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	hostname, err := os.Hostname()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Fprintln(w ,"Hostname: ", hostname)
+	fmt.Fprintln(w, "Hostname: ", hostname)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServe(":8888", nil))
+	port := getenv("SIMPLE_APP_PORT", "8888")
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 	fmt.Println(os.Hostname())
 }
